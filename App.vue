418b1e6f606fb71fd100d90d519a476896a7068f
essenija1 <80848058+essenija1@users.noreply.github.com>
2022-01-20 17:30:30 +0300

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,59 @@
+<template>
+<div id="App">
+  <h1>Todo application</h1>
+  <AddTodo 
+  @add-todo="addTodo"
+  />
+  <hr />
+  <TodoList 
+  v-bind:todos="todos"
+  @remove-todo="removeTodo"
+  />
+  <router-view />
+</div>
+
+</template>
+
+<script>
+import TodoList from "@/components/TodoList.vue";
+import AddTodo from "@/components/AddTodo.vue";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      todos: []
+    }
+  },
+  mounted() {
+    fetch('https://jsonplaceholder.typicode.com/todos/todos?_limit=3')
+    .then(response => response.json())
+    .then(json => {
+      this.todos = json
+    })
+  },
+  methods: {
+     removeTodo(id) {
+       this.todos = this.todos.filter(t => t.id !== id)
+     },
+     addTodo(todo) {
+       this.todos.push(todo)
+     }
+  },
+  components: {
+    TodoList,
+    AddTodo
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
